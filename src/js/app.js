b0758ca722697723935180816fd23563bf6eae9d
Maks <peregrim.mt@gmail.com>
2021-02-20 18:48:21 +0100

    Add class Booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function() {
@@ -110,6 +111,14 @@ const app = {
     });
   },
 
+  initBooking: function() {
+    //const thisApp = this;
+
+    const tableRez = document.querySelector(select.containerOf.booking);
+
+    new Booking(tableRez);
+  },
+
   init: function(){
     const thisApp = this;
     /*console.log('*** App starting ***');
@@ -121,6 +130,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();      
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
